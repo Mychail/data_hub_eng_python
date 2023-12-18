5b7820cfecb1bb368454705e56cee21e175a26c7
Chukwujekwu <anahchukwujekwu@gmail.com>
2023-12-18 14:04:21 +0100

    code for madlips game

---
+++ my_python/madlips_sample.py
@@ -0,0 +1,18 @@
+# An Awkward Encounter
+
+word = input("a word ending in ing: ")
+liquid = input("a type of liquid: ")
+food = input("a type of food: ")
+adjective_1 = input("an adjective: ")
+noun_1 = input("a noun: ")
+adverb_1 = input("an adverb: ")
+noun_2 = input("a noun: ")
+adjective_2 = input("an adjective: ")
+verb_1 = input("a verb: ")
+adverb_2 = input("an adverb: ")
+plural_noun_1 = input("a plural noun: ")
+plural_noun_2 = input("a plural noun: ")
+
+
+print(f"After {word}- shotguns at the range, drinking {liquid}\n by the pool, and a big {food} dinner, this is turning\n out to be a/an {adjective_1} night. I'm at some bar called the\n {noun_1} Lounge, and I am {adverb_1} smashed right now.\n We have {noun_2} service in a/an {adjective_2} VIP area,\n but there are no girls here. Well, I'm the groom, so I think I'll go {verb_1} some up. I can't {adverb_2} see with all\n the flashing {plural_noun_1} in this club, but I think there's a group\n of {plural_noun_2} in that corner.")
+
